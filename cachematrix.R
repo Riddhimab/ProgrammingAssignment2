9df4fe18046510e19c1dbaf8f27f36018d67710a
Riddhima Bhagavatula <riddhima.b@outlook.com>
2019-02-22 13:41:58 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <-function(m=matrix())
+        {
+                inv<-NULL;
+                setm<-function(a)
+                {
+                        m<<-a;
+                        inv<<-NULL;
+                }
+                ## This function allows you to set the value of the matrix
+                
+                getm<-function(m){return(m)}; 
+                # This function helps you retrieve/return the value of the matrix m
+                
+                setinverse<-function(inverse){inv<<-inverse}
+                # This function helps you set the value of the inverse
+                
+                getinverse<-function(inv){return(inv)};
+                # This function helps you return/retrieve the value of the inverse
+                return(list(setm=stem,getm=getm,setinverse=setinverse,getinverse=getinverse))
+        }
+        
 
-}
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverseofm<-x$getinverse()
+        if(!is.null(inverseofm)){return(inverseofm)}
+        #Gets data from cache
+        matrixdata<-x$getm()
+        inverse<-solve(matrixdata,...)
+        x$setinverse(inverseofm)
+        return(inverseofm)
+        #Assuming that the matrix is invertible(as stated in the question)
+        #this function finds the inverse of the matrix
 }
